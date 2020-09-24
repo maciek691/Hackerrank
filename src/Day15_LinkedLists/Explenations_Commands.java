0f2927b846d548a3a600bbe33c6a7d202f5596d4
Maciek Wachowski <maciekmaciek691@gmail.com>
2020-09-24 16:00:05 +0200

    Day 15

---
+++ src/Day15_LinkedLists/Explenations_Commands.java
@@ -0,0 +1,39 @@
+package Day15_LinkedLists;
+
+import java.util.LinkedList;
+
+public class Explenations_Commands {
+    public static void main(String[] args) {
+
+// Podobne do tabeli ale ma dynamiczną długośc a nie z góry określoną.
+
+        LinkedList<String> myLinkedList = new LinkedList<String>();
+
+// Add a node with data="First" to back of the (empty) list
+        myLinkedList.add("First");
+
+// Add a node with data="Second" to the back of the list
+        myLinkedList.add("Second");
+
+// Insert a node with data="Third" at front of the list
+        myLinkedList.addFirst("Third");
+
+// Insert a node with data="Fourth" at back of the list
+        myLinkedList.addLast("Fourth");
+
+// Insert a node with data="Fifth" at index 2
+        myLinkedList.add(2, "Fifth");
+
+// Print the list: [Third, First, Fifth, Second, Fourth]
+        System.out.println(myLinkedList);
+
+// Print the value at list index 2:
+        System.out.println(myLinkedList.get(2));
+
+// Empty the list
+        myLinkedList.clear();
+
+// Print the newly emptied list: []
+        System.out.println(myLinkedList);
+    }
+}
