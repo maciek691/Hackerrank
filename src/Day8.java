f026e7b0f18f540f87fbaf9aa3dcbfd521baae2c
Maciek Wachowski <maciekmaciek691@gmail.com>
2020-09-24 15:58:34 +0200

    added explanation

--- src/Day8.java
+++ src/Day8.java
@@ -4,14 +4,34 @@ import java.util.Scanner;
 
 public class Day8 {
 
-    // stworzenie książki telefonicznej korzystajac z map
-/*    3
-    sam 99912222
-    tom 11122222
-    harry 12299933
-    sam
-            edward
-    harry*/
+   /*  stworzenie książki telefonicznej korzystajac z map
+    Map is an interface that provides a blueprint for data structures that take (key, value) pairs and map keys to their associated
+    values (it's important to note that both the  and the  must be Objects and not primitives).
+
+    Here are a few Map methods you will find helpful for this challenge:
+
+    containsKey(Object key): Returns true if the map contains a mapping for key; returns false if there is
+     no such mapping.
+    get(Object key): Returns the value to which the key is mapped; returns null if there is no such mapping.
+    put(K key, V value): Adds the (Key, Value) mapping to the Map; if the key is already in the map,
+     the value is overwritten.
+
+    Create a Map of String Keys to String Values, implemented by the HashMap class
+    Map<String,String> myMap = new HashMap<String,String>();
+
+    Adds ("Hi","Bye") mapping to myMap
+    myMap.put("Hi", "Bye");
+
+    Print the Value mapped to from "Hi"
+    System.out.println(myMap.get("Hi"));
+
+    Replaces "Bye" mapping from "Hi" with "Bye!"
+    myMap.put("Hi", "Bye!");
+
+    Print the Value mapped to from "Hi"
+    System.out.println(myMap.get("Hi"));
+    */
+
     public static void main(String[] args) {
 
         Map<String,Integer> phoneBook = new HashMap<String,Integer>();
@@ -30,13 +50,6 @@ public class Day8 {
             } else {
                 System.out.println("Not found");
             }
-            /*while(scn.hasNext()){
-                String s = scn.next();
-                if (phoneBook.containsKey(s)) {
-                    System.out.println(s+"="+phoneBook.get(s));
-                } else{
-                    System.out.println("Not found");
-                }*/
             }
         }
     }
