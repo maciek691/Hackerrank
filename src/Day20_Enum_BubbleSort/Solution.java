65cb1672a66929fb2742a5377ca56c2aa5a816b6
Maciek Wachowski <maciekmaciek691@gmail.com>
2020-09-30 21:33:55 +0200

    update

--- src/Day20_Enum_BubbleSort/Solution.java
+++ src/Day20_Enum_BubbleSort/Solution.java
@@ -7,6 +7,7 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] a = new int[n];
+
         for (int a_i = 0; a_i < n; a_i++) {
             a[a_i] = in.nextInt();
         }
