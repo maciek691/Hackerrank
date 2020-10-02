f78ebdb25b5e18fb663b4250c1e4f925fade6c13
Maciek Wachowski <maciekmaciek691@gmail.com>
2020-10-02 15:18:08 +0200

    Day 22

---
+++ src/Day22/Tree.java
@@ -0,0 +1,8 @@
+package Day22;
+
+public interface Tree<D extends Comparable> {
+    public boolean isEmpty();
+    public int cardinality();
+    public boolean member(D elt);
+    public NonEmptyBST<D> add(D elt);
+}
